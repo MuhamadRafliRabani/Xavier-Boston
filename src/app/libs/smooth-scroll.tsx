5b4d32c37c18f56b2b6863ca3e0e0d81dc3fac33
Noirrr <muhamadraflirabani@gmail.com>
2025-06-20 20:41:42 +0700

    fix smooth scroll speed

--- src/app/libs/smooth-scroll.tsx
+++ src/app/libs/smooth-scroll.tsx
@@ -28,7 +28,7 @@ export default function SmoothScroll({
       root
       ref={lenisRef}
       options={{
-        duration: 1.1,
+        duration: 1.06,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         smoothWheel: true,
       }}
